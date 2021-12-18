23fb357d3f1c5dee7dc771cd81acb0e35eecb933
Tomaž Čuk <venooxpersonal@gmail.com>
2021-12-18 15:52:20 +0100

    Vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -3,8 +3,15 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "generic/ubuntu2004"
+  config.vm.network "forwarded_port", guest: 80, host: 8000
+  config.vm.network "forwarded_port", guest: 443, host: 8443
   config.vm.network "forwarded_port", guest: 8080, host: 8080
+  config.vm.network "forwarded_port", guest: 3389, host: 3389
   config.vm.provision "file", source: "gns3.service.systemd", destination: "$HOME/gns3.service.systemd"
   config.vm.provision "file", source: "gns3_server.conf", destination: "$HOME/.config/GNS3/2.2/gns3_server.conf"
+  config.vm.provision "file", source: "nginx_guacamole.conf", destination: "$HOME/nginx_guacamole.conf"
+  config.vm.provision "file", source: "server.xml", destination: "$HOME/server.xml"
+  config.vm.provision "file", source: "user-mapping.xml", destination: "$HOME/user-mapping.xml"
+  config.vm.provision "file", source: "GNS3.desktop", destination: "$HOME/GNS3.desktop"
   config.vm.provision "shell", path: "provisioning.sh"
 end
--- provisioning.sh
+++ provisioning.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Add repos
+# Enable firewall
+sudo ufw enable
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow ssh
+
+# Add repos for GNS3 and Docker
 sudo apt-get install ca-certificates curl gnupg lsb-release
 sudo add-apt-repository -y ppa:gns3/ppa
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -8,9 +14,10 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Install gns3, docker, firefox and desktop environment
+# Install gns3, docker, firefox and lightweight desktop environment LxQt
 sudo apt-get -y update && sudo apt -y upgrade
 sudo apt-get -y install debconf-utils
+# non-interactive selections
 sudo debconf-set-selections << EOF
 ubridge ubridge/install-setuid  boolean true
 wireshark-common        wireshark-common/install-setuid boolean true
@@ -30,11 +37,15 @@ sudo cp gns3.service.systemd /lib/systemd/system/gns3.service
 sudo chown root /lib/systemd/system/gns3.service
 sudo systemctl enable gns3
 
-# Preparing libraries for Guacamole installation
+# Run GNS3-GUI on startup
+sudo mkdir -p /home/vagrant/.config/autostart/
+mv /home/vagrant/GNS3.desktop /home/vagrant/.config/autostart/
+
+# Installing libraries for Guacamole server
 sudo apt-get -y install build-essential
 sudo apt-get -y install libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libssl-dev libwebp-dev
 
-## Configure and build Guacamole server
+# Download, configure and build Guacamole server
 wget https://dlcdn.apache.org/guacamole/1.3.0/source/guacamole-server-1.3.0.tar.gz
 tar -xzf guacamole-server-1.3.0.tar.gz
 cd guacamole-server-1.3.0/
@@ -47,10 +58,31 @@ cd
 # Install Java and Tomcat
 sudo apt-get -y install openjdk-11-jdk tomcat9
 sudo systemctl enable tomcat9
+sudo mv /home/vagrant/server.xml /var/lib/tomcat9/conf/server.xml
 
 # Guacamole client
 wget https://dlcdn.apache.org/guacamole/1.3.0/binary/guacamole-1.3.0.war
 sudo cp guacamole-1.3.0.war /var/lib/tomcat9/webapps/guacamole.war
 
+# Setup proxy for Guacamole client
+sudo apt-get -y install nginx
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt \
+  -subj "/C=SI/ST=Ljubljana/L=Ljubljana/O=FRI/OU=RK/CN=localhost"
+sudo curl https://ssl-config.mozilla.org/ffdhe2048.txt -o /etc/nginx/dhparam.pem
+sudo mv /home/vagrant/nginx_guacamole.conf /etc/nginx/sites-available/
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/nginx_guacamole.conf /etc/nginx/sites-enabled/
+
+# Guacamole client configuration
+sudo sh -c "echo 'GUACAMOLE_HOME=/etc/guacamole' >> /etc/default/tomcat9"
+sudo mkdir -p /etc/guacamole
+sudo mv /home/vagrant/user-mapping.xml /etc/guacamole/
+sudo sh -c "echo 'guacd-hostname: localhost' >> /etc/guacamole/guacamole.properties"
+sudo sh -c "echo 'guacd-port: 4822' >> /etc/guacamole/guacamole.properties"
+sudo sh -c "echo 'user-mapping: /etc/guacamole/user-mapping.xml' >> /etc/guacamole/guacamole.properties"
+
+# Setup RDP server
+sudo apt-get -y install xrdp
+sudo adduser xrdp ssl-cert
 
 sudo systemctl reboot
